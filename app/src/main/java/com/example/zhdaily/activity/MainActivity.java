4d7445d86f0925070b237cf3ce169effbcc56457
1439822216 <1439822216@qq.com>
2019-06-10 10:35:55 +0800

    4

--- app/src/main/java/com/example/zhdaily/activity/MainActivity.java
+++ app/src/main/java/com/example/zhdaily/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.zhdaily;
+package com.example.zhdaily.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.zhdaily.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
